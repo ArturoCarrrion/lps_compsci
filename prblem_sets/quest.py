6b1424bdb05907deb08b61c56706d940b618d35c
Arturo Carrion III <rc.carrion.arturo@leadps.org>
2016-11-17 01:45:07 +0000

    uptated

--- prblem_sets/quest.py
+++ prblem_sets/quest.py
@@ -1,16 +1,16 @@
-print('Welcome to arturo\'s Quest!')
+print('Welcome to Arturo\'s Quest!')
 
 print('Enter the name of your character:')
 charecter_name = raw_input()
 
 print('Enter strength (1-10):')
-strength = raw_input()
+strength = int(raw_input())
 
 print('Enter health (1-10):')
-health = raw_input()
+health = int(raw_input())
 
 print('Enter luck (1-10):')
-luck = raw_input()
+luck = int(raw_input())
 
 sum = int(luck) + int(strength) + int(health)
 
@@ -18,7 +18,7 @@ if sum <= 15:
 	print("your carecter's name is " + charecter_name + " your strength is " + str(strength) + " your health is " + str(health) + " and your luck is " + str(luck))
 
 if sum > 15:
-	print('You have give your character too many points! Default values have been assigned, Binky Bop:')
+	print('You have give your character too many points! Default values have been assigned, '+ charecter_name)
 
 if sum > 15:
 	strength = 5
@@ -27,9 +27,20 @@ if sum > 15:
 
 	luck = 5
 
-	print('strength: ' + str(strength) + ' ,health ' + str(health) + ' ,luck: ' + str(health))
+	print('strength: ' + str(strength) + ' ,health: ' + str(health) + ' ,luck: ' + str(luck))
 
 
-print('Binky Bop, you have come to a fork in the road. Do you want to go right or left?')
-print ('Enter right or left')
-direction = raw_input()
+print(charecter_name +' , you have come to a fork in the road. Do you want to go right or left?')
+print ('Enter right or left!')
+direction = str(raw_input())
+
+if direction == "left" and strength >= 4:
+	print('you ran into a cliff and need to climb down because its the only way to get to the other side you survive because your strength was high enough you win the game')
+if direction == "left" and strength < 4:
+	print('you ran in to a cliff and need to climb down because its the only way to get to the other side you did NOT servive because your strenght was not hig enuf you lose the game')
+
+if direction == "right" and health >= 5:
+	print('you have come across a river full of deadly fish you were able to servive because your health was ' + str(health) +' you win the game') 
+elif direction == "right" and health < 5:
+	print('you have come across a river full of deadly fish you were not able to servive because your health was to low, you lose the game')
+        
