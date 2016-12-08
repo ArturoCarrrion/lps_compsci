823f2eeaf6c0aebb1847a40ef618ed7ab01817ff
Arturo Carrion III <rc.carrion.arturo@leadps.org>
2016-12-08 17:28:22 +0000

    uptated p4

--- prblem_sets/guess.py
+++ prblem_sets/guess.py
@@ -9,12 +9,12 @@ number = 0
 #the next block of code is a while loop that will run if that stament is true wich is if the neumber that the user enters does not equal the same as the number that the computer chose. Also it lets the user enter there number.
 while number != num:
 	number = int(raw_input())
-#the next block is a if stament wich checks if its true or false so it checks if the number that the user enters is greater than the number that the computer chose. Also it lets the user know if there number is to high.
-	if number > num:
- 		print('to hight please enter another number:')
-#the next block is a elif stament wich checks if its true or false if the line above does not apply to it so it checks if the number that the user enters is less than the number that the computer chose and lets the user know if the number they enter is to low.  
-	elif number < num:
+#the next block is a if stament wich checks if its true or false if the line above does not apply to it so it checks if the number that the user enters is less than the number that the computer chose and lets the user know if the number they enter is to low.  
+	if number < num:
 		print('to low guess a gain enter your number:')
+#the next block is a elif stament wich checks if its true or false so it checks if the number that the user enters is greater than the number that the computer enters it will print the stament to try again.  
+        elif number > num:
+                print('to hight please enter another number:')
 #the next block of code is a else stament wich means that if any of the obove lines of code appply to the users input it will say that they won the game because they guess the corect number. 			
 	else:
 		print('you won the game!!!')
